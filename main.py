df8aa28ff6a6c44ad738beb31fbc93e118406705
zlbd <791597507@qq.com>
2023-12-01 01:06:37 +0800

    add doc

--- main.py
+++ main.py
@@ -1,139 +1,89 @@
 # !/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 # @Author  : zlbd
+# This is a sample Python script.
 
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By     #导入By类
+from selenium.webdriver.support.ui import WebDriverWait     #导入显示等待类
+from selenium.webdriver.support import expected_conditions  #导入期望场景类
 
-wd = webdriver.Chrome()
-wd.implicitly_wait(10)
-
-wd.get('https://cn.bing.com/search?q=%e7%99%be%e5%ba%a6&qs=SC&pq=baidu&sc=10-5&cvid=55131F227A304C3CB96FFFC540CB1431&FORM=QBLH&sp=1&lq=0')
-time.sleep(1)
-# 点击打开新窗口的链接
-link = wd.find_element(By.XPATH, '//*[@id="b_results"]/li[1]/h2/a')
-link.click()
-mainWindow = wd.current_window_handle
-for handle in wd.window_handles:
-    # 先切换到该窗口
-    wd.switch_to.window(handle)
-    # 得到该窗口的标题栏字符串，判断是不是我们要操作的那个窗口
-    if '百度一下' in wd.title:
-        # 如果是，那么这时候WebDriver对象就是对应的该该窗口，正好，跳出循环，
-        break
-
-# wd.title属性是当前窗口的标题栏 文本
-print(wd.title)
-wd.switch_to.window(mainWindow)
-time.sleep(1)
-print(wd.title)
+"""
+BeautifulSoup4将复杂HTML文档转换成一个复杂的树形结构,每个节点都是Python对象,所有对象可以归纳为4种:
+（1）Tag
+（2）NavigableString
+（3）BeautifulSoup
+（4）Comment
+"""
+from bs4 import BeautifulSoup
 
 
-import time
+def comments():
+    """
+    元素查找方法汇总
+    find_element_by_name
+    find_element_by_id
+    find_element_by_xpath
+    find_element_by_link_text
+    find_element_by_partial_link_text
+    find_element_by_tag_name
+    find_element_by_class_name
+    find_element_by_css_selector
+    以上是单元素查找，多元素把 element 变成 elements 即可。
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-wd = webdriver.Chrome()
-wd.implicitly_wait(10)
-def isAscending(xs):
-    for n in range(len(xs) - 1):
-        if xs[n] > xs[n+1]:
-            return False
-    return True
-wd.get('https://www.uniqlo.cn/c/4221124018.html')
-time.sleep(10)
-# 点击打开新窗口的链接
-element = wd.find_element(By.XPATH,"//*[@style='display: flex;']").click()
-wd.find_element(By.XPATH, "//*[@class='sort-content']/*[3]").click()
-time.sleep(5)
-price = wd.find_elements(By.XPATH,'//*[@id="hmall-container"]/div/div[1]/div[2]/div[1]/div/div/div/div/div[5]/div/div[2]/div/div[1]/div/a/div/span')
-all=[]
-for ele in price:
-    all.append(ele.text)
-print(all)
-assert isAscending(all) is True
+    还有一种较通用的方法
+    from selenium.webdriver.common.by import By    注意这里要导入
+    input_first = browser.find_element(By.ID,"q")    ID可以换成其他
+    """
+    pass
 
 
+def main():
+    # 1. 获取浏览器对象
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')  #可设定无界面模式，即操作浏览器时，隐层浏览器, 设置无界面  可选
+    browser = webdriver.Chrome(options = options)
+    print("hello world")
+    browser.implicitly_wait(10)  # # 智能等待，在设置时间范围内，只要条件成立，马上结束等待, implicitly_wait
 
-class DialogAddTagXXX(object):
+    # 2. 访问页面
+    browser.get('https://cn.bing.com/')
+    #browser.minimize_window()  # 窗口最小化，窗口最大化用 wd.maximize_window()，可有可无，看情况
 
-    def __init__(self):
-        pass
+    # 3. 查找某元素
+    element = browser.find_element(By.ID, "est_cn")
+    print(element)
+    print(element.text)
 
-    def get_grid_bodies(self, locator):
-            """
-            :param locator: tuple. eg: ("By.XPATH", r"//ag-grid-angular[@id='myGrid']" layer)
-            :return bodies: list.
-            """
-            # step1:
-            # Get locator.
-            body_locator = (locator[0], locator[1] + r"//div[contains(@class, 'ag-center-cols-container')]/div")
-            # step2:
-            # Get body elements.
-            elements = self.common.find_elements(body_locator)
-            # step3:
-            # Get bodies text.
-            if len(elements) == 0:
-                return []
-            else:
-                bodies = []
-                for _line in elements:
-                    _div_index = int(elements.index(_line)) + 1
-                    _line_locator = (locator[0], body_locator[1] + "/../div[%s]/div" % _div_index)
-                    _line_subelements = self.common.find_elements(_line_locator)
-                    _line_items = [_ele.text for _ele in _line_subelements]
-                    bodies.append(_line_items)
-                return bodies
+    # 4. 拟按键输入，查找元素并交互模
+    xpathContent = "/html/body/div[2]/div/div[3]/div[2]/form/div[1]/input"
+    element = browser.find_element(By.XPATH, xpathContent)
+    if element:
+        element.send_keys("selenium")
+        element.submit()
 
-        def get_grid_datalist(self, locator):
-            """
-            :param locator: tuple. eg: ("By.XPATH", r"//ag-grid-angular[@id='myGrid']" layer)
-            :return _datalist: list.
-            """
-            # step1:
-            # Get grid headers.
-            headers = self.get_grid_headers(locator)
-            # step2:
-            # Get grid bodies.
-            bodies = self.get_grid_bodies(locator)
-            # step3:
-            # Get grid datalist.
-            datalist = []
-            for body in bodies:
-                _dict = {}
-                for head in headers:
-                    _index = headers.index(head)
-                    _dict[head] = body[_index]
-                datalist.append(_dict)
-            return datalist
+    try:
+        # 最多等待10秒直到浏览器标题栏中出现我希望的字样（比如查询关键字出现在浏览器的title中）
+        WebDriverWait(browser, 10).until(
+            expected_conditions.title_contains('selenium'))
+        print(browser.title)
 
-        def select_row_by_index(self, locator, index):
-            """
-            Select row checkbox by index.
-            :param locator: tuple. eg: ("By.XPATH", r"<span[@class='ag-selection-checkbox']> layer")
-            :param index: int. start from 1.
-            :return:
-            """
-            # step1:
-            # Get locator.
-            with_index_locator = (locator[0], locator[1] % index)
-            status_locator = (with_index_locator[0], with_index_locator[1] + r'//input')
-            # step2:
-            # Select row checkbox.
-            status = self.common.find_element(status_locator).is_selected()
-            if not status:
-                self.common.click_element(locator)
+        bsobj = BeautifulSoup(browser.page_source, "html.parser")
+        print(type(bsobj.text))
 
-        def get_visible_bodies_from_tagXXX_grid(self):
-            """
-            Get visible bodies from 'tagXXX' grid.
-            """
-            # step1:
-            # Get locator.
-            _grid_tagXXX_locator = self.Dialog_AT_Pane.grid_tagXXX()
-            # step2:
-            # Get tags browsed.
-            grid_bodies = self.GridActor.get_grid_bodies(_grid_tagXXX_locator)
-            return grid_bodies
+        num_text_element = bsobj.find('span', {'class': 'nums_text'})
+        print(num_text_element)
+
+    finally:
+        # 关闭浏览器
+        browser.close()
+
+    print('----------------done!!------------------')
+
+
+if __name__ == '__main__':
+    main()
+    print('----------------done!------------------')
+
+# EOF
